f67436b171492598f775a469e31fd10f895bc954
Davi Eduardo Gama Barbosa <davieduardo94@gmail.com>
2022-05-02 23:39:23 -0400

    trying validates for #1

--- buscar_cep/views.py
+++ buscar_cep/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
+import re
 
 
 def index(request):
@@ -11,19 +12,35 @@ def consulta_api(request):
     url = f'https://viacep.com.br/ws/'
     formato = '/json/'
     ret = ''
-    if 'buscar' in request.GET:
-        cep = request.GET['buscar']
-        data = requests.get(url+cep+formato)
-        ret = data.json()
     dados = {}
-    for key, value in enumerate (ret):
-        dados[key] = value
+
+    if 'buscar' in request.GET:
+        cep = request.GET['buscar'].replace('-','')
+        if len(cep) == 8:
+            regex = r"^[0-9]+$"
+            
+            # realizando requisição na API com CEP informado
+            data = requests.get(url+cep+formato)
+            # recebendo dados no formato json
+            ret = data.json()
+        
+            # percorrendo as key, values do json e transfornado em dict
+            for key, value in enumerate (ret):
+                dados[key] = value
+
+            context = {
+                'key_cep' : ret['cep'],
+                'key_rua' : ret['logradouro'],
+                'key_complemento' : ret['complemento'],
+                'key_bairro' : ret['bairro'],
+                'key_localidade' : ret['localidade'],
+                'key_uf' : ret['uf'],
+                'key_ddd' : ret['ddd'],
+            }
+    else:
+        context  = {
+            'result' : 'O CEP NÃO FOI ENCONTRADO OU NÃO INFORMADO CORRETAMENTE!'
+        }
     
-    context = {
-        'cep' : ret['cep'],
-        'rua' : ret['logradouro'],
-        'complemento' : ret['complemento'],
-        'bairro' : ret['bairro'],
-    }
     return render(request, 'resultado.html', context)
 
